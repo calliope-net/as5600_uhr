01a4c3abd67b5be97278703711129a28b137b4e5
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-07-30 12:59:56 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    motor_on = !(motor_on)
+    if (!(motor_on)) {
+        motors.motorPower(0)
+        basic.turnRgbLedOff()
+    }
+})
 function raw_angle () {
     bu = pins.pins_i2cWriteReadBuffer(pins.pins_i2cAdressen(pins.ei2cAdressen.AS5600), pins.buffer_fromArray([12]), 2)
     if (bu) {
@@ -6,41 +13,56 @@ function raw_angle () {
         return 0
     }
 }
+let blink = false
+let current_speed = 0
 let speed = 0
 let differenz = 0
 let rotary_4096 = 0
 let minute_4096 = 0
 let bu: Buffer = null
+let motor_on = false
+motor_on = false
 pins.addDisplay(pins.pins_DigitalPin(DigitalPin.C16), pins.pins_DigitalPin(DigitalPin.C17))
-let current_speed = 0
+pins.oled_reset(pins.oled_pages.y64)
+pins.oled_write_text(0, 0, 15, pins.pins_text("Magnetic Rotary"))
+pins.oled_write_text(1, 0, 15, pins.pins_text("Position Sensor"))
+pins.oled_write_text(4, 0, 5, pins.pins_text("minute"))
+pins.oled_write_text(5, 0, 5, pins.pins_text("rotary"))
+let go = true
 basic.forever(function () {
-    pins.rtc_read()
-    pins.zeigeDoppelpunkt(true)
-    pins.zeigeText(pins.rtc_get_string(pins.rtc_eFormat.hhmm))
-    basic.pause(1000)
-    minute_4096 = 2048 * (pins.rtc_get_int(pins.pins_rtc_eRegister(pins.rtc_eRegister.Stunde)) % 12 * 60 + pins.rtc_get_int(pins.pins_rtc_eRegister(pins.rtc_eRegister.Minute))) / 360
-    pins.zeigeDoppelpunkt(false)
-    pins.zeigeZahl(Math.round(minute_4096))
-    basic.pause(1000)
-    rotary_4096 = raw_angle()
-    pins.zeigeZahl(Math.round(rotary_4096))
-    differenz = pins.minx(minute_4096, rotary_4096)
-    if (Math.abs(differenz) >= 5) {
-        if (differenz < 0) {
-            speed = -100
+    if (go) {
+        pins.rtc_read()
+        pins.zeigeText(pins.rtc_get_string(pins.rtc_eFormat.hhmm))
+        pins.oled_write_text(2, 2, 15, pins.pins_text(pins.rtc_get_string(pins.rtc_eFormat.yyMMddHHmmss)))
+        minute_4096 = 2048 * (pins.rtc_get_int(pins.pins_rtc_eRegister(pins.rtc_eRegister.Stunde)) % 12 * 60 + pins.rtc_get_int(pins.pins_rtc_eRegister(pins.rtc_eRegister.Minute))) / 360
+        pins.oled_write_text(4, 7, 15, pins.roundWithPrecision(minute_4096, 1))
+        rotary_4096 = raw_angle()
+        pins.oled_write_text(5, 7, 15, rotary_4096)
+        differenz = pins.minx(minute_4096, rotary_4096)
+        pins.oled_write_text(6, 7, 15, pins.roundWithPrecision(differenz, 1))
+        if (Math.abs(differenz) >= 5) {
+            if (differenz < 0) {
+                speed = -100
+                basic.setLedColor(0x0000ff)
+            } else {
+                speed = 100
+                basic.setLedColor(0x00ff00)
+            }
         } else {
-            speed = 100
+            speed = 0
+            basic.setLedColor(0xff0000)
         }
-    } else {
-        speed = 0
-    }
-    if (current_speed != speed) {
-        current_speed = speed
-        motors.motorPower(speed)
-    }
-    if (speed == 0) {
-        basic.pause(1000)
-    } else {
-        basic.pause(1000)
+        pins.oled_write_text(7, 7, 10, current_speed, pins.oled_align.right)
+        pins.oled_write_text(7, 12, 15, speed, pins.oled_align.right)
+        if (motor_on && current_speed != speed) {
+            current_speed = speed
+            motors.motorPower(speed)
+        }
+        blink = !(blink)
+        if (speed == 0) {
+            basic.pause(1000)
+        } else {
+            basic.pause(500)
+        }
     }
 })
