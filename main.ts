6ae7a45132d5a7f19ac3f1c6ba10a652246beb1b
calliope-net <141074597+calliope-net@users.noreply.github.com>
2026-07-30 19:12:09 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,24 @@ function uhr_stellen () {
         pins.oled_write_text(3, 0, 5, pins.rtc_set_key(pins.keypad_read()))
     }
 }
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (go) {
+        go = false
+        basic.pause(2000)
+        pins.oled_write_text(4, 7, 15, pins.pins_text("AS5600"), pins.oled_align.right)
+        bu = pins.pins_i2cWriteReadBuffer(pins.pins_i2cAdressen(pins.ei2cAdressen.AS5600), pins.buffer_fromArray([11]), 1)
+        if (bu) {
+            pins.oled_write_text(5, 7, 15, pins.pins_text("STA 0x" + pins.buffer_toHex(bu)))
+        }
+        bu = pins.pins_i2cWriteReadBuffer(pins.pins_i2cAdressen(pins.ei2cAdressen.AS5600), pins.buffer_fromArray([26]), 3)
+        if (bu) {
+            pins.oled_write_text(6, 7, 15, "AGC " + pins.buffer_getUint8(bu, 0))
+            pins.oled_write_text(7, 7, 15, pins.pins_text("MAG " + pins.buffer_getNumber(bu, NumberFormat.UInt16BE, 1)))
+        }
+    } else {
+        go = true
+    }
+})
 function raw_angle () {
     bu = pins.pins_i2cWriteReadBuffer(pins.pins_i2cAdressen(pins.ei2cAdressen.AS5600), pins.buffer_fromArray([12]), 2)
     if (bu) {
@@ -21,7 +39,11 @@ function raw_angle () {
 function blinken () {
     blink = !(blink)
     if (blink) {
-        basic.setLedColor(0x00ff00)
+        if (motor_on) {
+            basic.setLedColor(0x00ff00)
+        } else {
+            basic.setLedColor(0xff0000)
+        }
     } else {
         basic.setLedColor(0x0000ff)
     }
@@ -34,15 +56,19 @@ let minute_4096 = 0
 let blink = false
 let bu: Buffer = null
 let motor_on = false
+let go = false
+go = false
 motor_on = false
-pins.addDisplay(pins.pins_DigitalPin(DigitalPin.C16), pins.pins_DigitalPin(DigitalPin.C17))
-pins.oled_reset(pins.oled_pages.y64)
-pins.oled_write_text(0, 0, 15, pins.pins_text("Magnetic Rotary"))
-pins.oled_write_text(1, 0, 15, pins.pins_text("Position Sensor"))
-pins.oled_write_text(4, 0, 5, pins.pins_text("minute"))
-pins.oled_write_text(5, 0, 5, pins.pins_text("rotary"))
-pins.oled_write_text(7, 0, 5, pins.pins_text("speed"))
-let go = true
+if (!(pins.simulator())) {
+    pins.addDisplay(pins.pins_DigitalPin(DigitalPin.C16), pins.pins_DigitalPin(DigitalPin.C17))
+    pins.oled_reset(pins.oled_pages.y64)
+    pins.oled_write_text(0, 0, 15, pins.pins_text("Magnetic Rotary"))
+    pins.oled_write_text(1, 0, 15, pins.pins_text("Position Sensor"))
+    pins.oled_write_text(4, 0, 5, pins.pins_text("minute"))
+    pins.oled_write_text(5, 0, 5, pins.pins_text("rotary"))
+    pins.oled_write_text(7, 0, 5, pins.pins_text("speed"))
+    go = true
+}
 basic.forever(function () {
     if (go) {
         uhr_stellen()
@@ -65,7 +91,7 @@ basic.forever(function () {
             speed = 0
         }
         pins.oled_write_text(7, 7, 10, current_speed, pins.oled_align.right)
-        pins.oled_write_text(7, 12, 15, speed, pins.oled_align.right)
+        pins.oled_write_text(7, 11, 15, speed, pins.oled_align.right)
         if (motor_on && current_speed != speed) {
             current_speed = speed
             motors.motorPower(speed)
